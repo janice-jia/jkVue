f53cb5996eb516cf82219d3497b6a94161d6b782
Jason <jiameng@jiamengdeMacBook-Air.local>
2018-11-12 02:07:51 +0800

    配置本地代理

--- config/proxyConfig.js
+++ config/proxyConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
     proxy: {
-          '/api/aAPI.ashx': {    
+          '/api': {    
               target: 'http://www.9kuaiz.com/',  // 接口域名
               secure: false,  // 如果是https接口，需要配置这个参数
               changeOrigin: true,  //是否跨域
