ccf96e2f9148ff3ee07d1162343965964e6b79ac
jiamengpei <jiamengpei.bj@acewill.cn>
2018-11-17 10:55:57 +0800

    底部统一menu设置选中样式

--- src/components/bottomCom.vue
+++ src/components/bottomCom.vue
@@ -1,17 +1,17 @@
 <template>
     <!-- 底部导航 start-->
     <div class="jk-nav jkFlex">
-        <div class="jk-nav-item jkFlexItem nav-hover">
+        <div v-bind:class="[routeName=='index' ? 'jk-nav-item jkFlexItem nav-hover' : 'jk-nav-item jkFlexItem']">
             <router-link class="houseLink" :to="{name: 'index'}"></router-link>
             <div class="jk-nav-item-icon nav-icon01"></div>
             <p>首页</p>
         </div>
-        <div class="jk-nav-item jkFlexItem">
+        <div v-bind:class="[routeName=='publish' ? 'jk-nav-item jkFlexItem nav-hover' : 'jk-nav-item jkFlexItem']">
             <router-link class="houseLink" :to="{name: 'publish'}"></router-link>
             <div class="jk-nav-item-icon nav-icon02"></div>
             <p>发布</p>
         </div>
-        <div class="jk-nav-item jkFlexItem">
+        <div  v-bind:class="[routeName=='mine' ? 'jk-nav-item jkFlexItem nav-hover' : 'jk-nav-item jkFlexItem']">
             <router-link class="houseLink" :to="{name: 'mine'}"></router-link>
             <div class="jk-nav-item-icon nav-icon03"></div>
             <p>我</p>
@@ -22,9 +22,11 @@
 
 <script>
 export default {
-  name: 'bottomCom',
-  data () {
-      return {}
-  }
+    name: 'bottomCom',
+    data () {
+      return {
+          routeName: this.$route.name
+      }
+    }
 }
 </script>
